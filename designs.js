942842c6a78589a63f0c5e121c15942834f2d8c6
peculiar 907 <peculiar907@gmail.com>
2022-04-22 14:10:53 -0700

    3rd commit

--- designs.js
+++ designs.js
@@ -1,45 +1,34 @@
-var canvas= document.getElementById ('pixelCanvas');
-var sizePicker = document.getElementById('sizePicker');
-var gridHeight = document.getElementById ('inputHeight');
-var gridWidth =   document.getElementById('inputWidth');
-var colorPick = document.getElementById ('colorPicker');
-
-
-
+var canvas = document.getElementById("pixelCanvas");
+var sizePicker = document.getElementById("sizePicker");
+var gridHeight = document.getElementById("inputHeight");
+var gridWidth = document.getElementById("inputWidth");
+var colorPick = document.getElementById("colorPicker");
 
 // When size is submitted by the user, call makeGrid()
 
-sizePicker.addEventListener( 'submit', function(event) {
-    event.preventDefault();
-    
+sizePicker.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    // if gridCells is present , clear the grids that have been filled in
-    while ( canvas.firstChild ){
-        canvas.removeChild(canvas.firstChild)
-    }
-    
-    makeGrid();
-})
+  // if gridCells is present , clear the grids that have been filled in
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
 
- // function to create gridCells and gridRows
+  makeGrid();
+});
+
+// function to create gridCells and gridRows
 
 function makeGrid() {
-   for (let j = 0; j< gridHeight.value; j++) {
-       let gridRow = canvas.insertRow(j);
-       for (let k = 0; k < gridWidth.value; k++) {
-         let gridCell = gridRow.insertCell(k);
-         
-       //  fills gridCells with selected color
-         gridCell.addEventListener('mousedown', function(){
-             gridCell.style.backgroundColor = colorPick.value;
-         })
-         
-        
-        }
-      
-   }
-      
+  for (let j = 0; j < gridHeight.value; j++) {
+    let gridRow = canvas.insertRow(j);
+    for (let k = 0; k < gridWidth.value; k++) {
+      let gridCell = gridRow.insertCell(k);
+
+      //  fills gridCells with selected color
+      gridCell.addEventListener("mousedown", function () {
+        gridCell.style.backgroundColor = colorPick.value;
+      });
+    }
+  }
 }
-
-
-
